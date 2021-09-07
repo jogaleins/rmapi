bfe188cd573c0e8869dc24e0f0aaff04681a9fb5
ap <ap@joga.duckdns.org>
2021-09-07 19:54:38 +0800

    added jwt

--- src/app.py
+++ src/app.py
@@ -1,23 +1,38 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow 
+from werkzeug.security import generate_password_hash,check_password_hash
+from functools import wraps
+import uuid
+import jwt
+import datetime
 import os
+
+
 #import pymysql
 
 # Init app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://k3s:k3s_123@192.168.1.200/rm'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://k3s:k3s_123@192.168.1.200/rm'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY']='bf6e4aea32a10e7c7df30d88bd3b1b85'
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
 db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
 
 # Package Class/Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    public_id = db.Column(db.String(50), unique = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(70), unique = True)
+    password = db.Column(db.String(80))
+
 class Package(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   package = db.Column(db.String(100), unique=True)
@@ -44,10 +59,131 @@ class PackageSchema(ma.Schema):
 package_schema = PackageSchema()
 packages_schema = PackageSchema(many=True)
 
-#
+# decorator for verifying the JWT
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+  
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query\
+                .filter_by(public_id = data['public_id'])\
+                .first()
+        except:
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users contex to the routes
+        return  f(current_user, *args, **kwargs)
+  
+    return decorated
+
+#Routes
 @app.route('/rmapi')
 def hello():
   return 'api root'
+
+# route for logging user in
+@app.route('/rmapi/login', methods =['POST'])
+def login():
+    # creates dictionary of form data
+    auth = request.form
+  
+    if not auth or not auth.get('email') or not auth.get('password'):
+        # returns 401 if any email or / and password is missing
+        return make_response(
+            'Could not verify',
+            401,
+            {'WWW-Authenticate' : 'Basic realm ="Login required !!"'}
+        )
+  
+    user = User.query\
+        .filter_by(email = auth.get('email'))\
+        .first()
+  
+    if not user:
+        # returns 401 if user does not exist
+        return make_response(
+            'Could not verify',
+            401,
+            {'WWW-Authenticate' : 'Basic realm ="User does not exist !!"'}
+        )
+  
+    if check_password_hash(user.password, auth.get('password')):
+        # generates the JWT Token
+        token = jwt.encode({
+            'public_id': user.public_id,
+            'exp' : datetime.utcnow() + timedelta(minutes = 30)
+        }, app.config['SECRET_KEY'])
+  
+        return make_response(jsonify({'token' : token.decode('UTF-8')}), 201)
+    # returns 403 if password is wrong
+    return make_response(
+        'Could not verify',
+        403,
+        {'WWW-Authenticate' : 'Basic realm ="Wrong Password !!"'}
+    )
+  
+# signup route
+@app.route('/rmapi/signup', methods =['POST'])
+def signup():
+    # creates a dictionary of the form data
+    data = request.form
+  
+    # gets name, email and password
+    name, email = data.get('name'), data.get('email')
+    password = data.get('password')
+  
+    # checking for existing user
+    user = User.query\
+        .filter_by(email = email)\
+        .first()
+    if not user:
+        # database ORM object
+        user = User(
+            public_id = str(uuid.uuid4()),
+            name = name,
+            email = email,
+            password = generate_password_hash(password)
+        )
+        # insert user
+        db.session.add(user)
+        db.session.commit()
+  
+        return make_response('Successfully registered.', 201)
+    else:
+        # returns 202 if user already exists
+        return make_response('User already exists. Please Log in.', 202)
+
+#Get All users
+@app.route('/rmapi/users', methods=['GET'])
+@token_required
+def get_all_users(current_user):
+    # querying the database
+    # for all the entries in it
+    users = User.query.all()
+    # converting the query objects
+    # to list of jsons
+    output = []
+    for user in users:
+        # appending the user data json
+        # to the response list
+        output.append({
+            'public_id': user.public_id,
+            'name' : user.name,
+            'email' : user.email
+        })
+  
+    return jsonify({'users': output})
+
 # Create a Package
 @app.route('/rmapi/package', methods=['POST'])
 def new_package():
@@ -109,4 +245,4 @@ def delete_package(id):
   return package_schema.jsonify(package)
 # Run Server
 if __name__ == '__main__':
-  app.run(debug=False)
+  app.run(debug=True)
